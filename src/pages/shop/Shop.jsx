e142681637acfde1ba084f810ff22e7898c8004c
Joel Williams <109452946+JoelWilliams7@users.noreply.github.com>
2023-04-18 00:56:36 -0400

    recovery

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { PRODUCTS } from "../../Products";
+import Product from "./product";
+import "./shop.css";
+
+function Shop() {
+  return (
+    <div className="shop">
+      <div className="shop">
+        <h1>J's Electrical</h1>
+      </div>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
